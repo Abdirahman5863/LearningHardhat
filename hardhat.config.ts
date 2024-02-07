0bde1ffb9b3403a9b2fb59de7fcf6360f03ecd67
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-02-07 13:09:36 +0300

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [process.env.SEPOLIA_API_KEY || ""],
+    },
+  },
 };
-
 export default config;
